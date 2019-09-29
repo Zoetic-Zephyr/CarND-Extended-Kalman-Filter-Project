d37e82b5ca6b614187fcab795d409c77075b66dc
Zoetic-Zephyr <31701275+Zoetic-Zephyr@users.noreply.github.com>
2019-09-29 23:55:41 +0800

    Complete kalman_filter implementation

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -12,30 +12,72 @@ KalmanFilter::KalmanFilter() {}
 
 KalmanFilter::~KalmanFilter() {}
 
-void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
-                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+Tools KalmanFilter::tools_ = Tools();
+
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in, MatrixXd &H_in,
+                        MatrixXd &R_lidar_in, MatrixXd &R_radar_in, MatrixXd &Q_in) {
   x_ = x_in;
   P_ = P_in;
   F_ = F_in;
   H_ = H_in;
-  R_ = R_in;
+  R_lidar = R_lidar_in;
+  R_radar = R_radar_in;
   Q_ = Q_in;
 }
 
 void KalmanFilter::Predict() {
-  /**
-   * TODO: predict the state
-   */
+  // TODO: predict the state
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Kalman Filter equations
-   */
+  // TODO: update the state by using Kalman Filter equations
+  // using equations for kalman filter
+  VectorXd y = z - H_ * x_;
+
+  // ! H_ and R_lidar
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_lidar;
+  MatrixXd Si = S.inverse();
+  MatrixXd K =  P_ * Ht * Si;
+
+  // new state
+  x_ = x_ + (K * y);
+  int x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Extended Kalman Filter equations
-   */
+  // TODO: update the state by using Extended Kalman Filter equations
+  MatrixXd Hj = tools_.CalculateJacobian(x_);
+
+  // using equations for extended kalman filter
+  // convert radar measurements from cartesian coordinates (x, y, vx, vy) to polar (rho, phi, rho_dot).
+  VectorXd x_polar = tools_.ConvertFromCartesianToPolarCoords(x_);
+  
+  VectorXd y = z - x_polar;
+
+  // normalize the angle between -pi to pi
+  while(y(1) > M_PI){
+    y(1) -= 2 * M_PI;
+  }
+
+  while(y(1) < -M_PI){
+    y(1) += 2 * M_PI;
+  }
+
+  // ! Hj and R_radar
+  MatrixXd Ht = Hj.transpose();
+  MatrixXd S = Hj * P_ * Ht + R_radar;
+  MatrixXd Si = S.inverse();
+  MatrixXd K =  P_ * Ht * Si;
+
+  // new state
+  x_ = x_ + (K * y);
+  int x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * Hj) * P_;
 }
--- src/kalman_filter.h
+++ src/kalman_filter.h
@@ -2,9 +2,32 @@
 #define KALMAN_FILTER_H_
 
 #include "Eigen/Dense"
+#include "tools.h"
 
 class KalmanFilter {
- public:
+public:
+
+  // state vector
+  Eigen::VectorXd x_;
+
+  // state covariance matrix
+  Eigen::MatrixXd P_;
+
+  // state transition matrix
+  Eigen::MatrixXd F_;
+
+  // process covariance matrix
+  Eigen::MatrixXd Q_;
+
+  // measurement matrix
+  Eigen::MatrixXd H_;
+
+  // measurement covariance matrix for lidar
+  Eigen::MatrixXd R_lidar;
+
+  // measurement covariance matrix for radar
+  Eigen::MatrixXd R_radar;
+
   /**
    * Constructor
    */
@@ -21,11 +44,12 @@ class KalmanFilter {
    * @param P_in Initial state covariance
    * @param F_in Transition matrix
    * @param H_in Measurement matrix
-   * @param R_in Measurement covariance matrix
+   * @param R_lidar_in Measurement covariance matrix for lidar
+   * @param R_radar_in Measurement covariance matrix for radar
    * @param Q_in Process covariance matrix
    */
-  void Init(Eigen::VectorXd &x_in, Eigen::MatrixXd &P_in, Eigen::MatrixXd &F_in,
-            Eigen::MatrixXd &H_in, Eigen::MatrixXd &R_in, Eigen::MatrixXd &Q_in);
+  void Init(Eigen::VectorXd &x_in, Eigen::MatrixXd &P_in, Eigen::MatrixXd &F_in, MatrixXd &H_in,
+            MatrixXd &R_lidar_in, MatrixXd &R_radar_in, Eigen::MatrixXd &Q_in);
 
   /**
    * Prediction Predicts the state and the state covariance
@@ -46,23 +70,9 @@ class KalmanFilter {
    */
   void UpdateEKF(const Eigen::VectorXd &z);
 
-  // state vector
-  Eigen::VectorXd x_;
-
-  // state covariance matrix
-  Eigen::MatrixXd P_;
-
-  // state transition matrix
-  Eigen::MatrixXd F_;
-
-  // process covariance matrix
-  Eigen::MatrixXd Q_;
-
-  // measurement matrix
-  Eigen::MatrixXd H_;
-
-  // measurement covariance matrix
-  Eigen::MatrixXd R_;
+private:
+  // tools class instance
+  static Tools tools_;
 };
 
 #endif // KALMAN_FILTER_H_
